2cfd669d8a6afc62f904e815e1326f50bd5365bf
Giulio Santoli <giulio.santoli@posteitaliane.it>
2019-08-25 23:28:55 +0200

    no need to revert data in main.go

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func getEnv(key, fallback string) string {
@@ -14,26 +12,6 @@ func getEnv(key, fallback string) string {
 		return value
 	}
 	return fallback
-}
-
-func revert(txt string) string {
-	words := strings.Split(txt, " ")
-
-	newWords := make([]string, len(words))
-
-	for i := 0; i < len(words); i++ {
-		newWords[i] = words[len(words)-1-i]
-	}
-
-	return strings.Join(newWords, " ")
-}
-
-type reverseRequest struct {
-	MessageContent string `json:"content"`
-}
-
-type reverseResponse struct {
-	ResponseResult string `json:"result"`
 }
 
 func main() {
@@ -44,33 +22,6 @@ func main() {
 	server.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("hello invoked")
 		fmt.Fprintf(w, "Hello World!")
-	})
-
-	// simple api: getting {"content": "some text"}, it returns {"result": "texts some"}
-	server.HandleFunc("/revert", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("revert invoked - ", r.Method)
-
-		if r.Method == http.MethodPost {
-			msg := reverseRequest{}
-
-			if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
-				log.Println("bad request, error: ", err)
-				w.WriteHeader(http.StatusBadRequest)
-			} else if msg.MessageContent != "" {
-				rsp := reverseResponse{ResponseResult: revert(msg.MessageContent)}
-
-				log.Println("revering:", msg.MessageContent, "->", rsp.ResponseResult)
-
-				w.Header().Set("Content-Type", "application/json; charset=utf-8")
-				w.WriteHeader(http.StatusOK)
-				json.NewEncoder(w).Encode(rsp)
-			} else {
-				w.WriteHeader(http.StatusBadRequest)
-			}
-
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
-		}
 	})
 
 	// static contents
